7212b512518d3c4a672c7855f1e33ee3da7477d0
zembahk <77575523+zembahk@users.noreply.github.com>
2022-07-30 14:42:20 -0700

    Update App.tsx
    buttons

--- src/App.tsx
+++ src/App.tsx
@@ -8,49 +8,45 @@ function App() {
   const { authenticate, isAuthenticated, isAuthenticating, user, logout } = useMoralis();
 
   useEffect(() => {
-    const loginButton = document.getElementById('login_btn') as HTMLButtonElement | null;
+    
     if (isAuthenticated) {
-      if (loginButton != null) {
-        loginButton.disabled = true;
-      }
-    }
-    else {
-      loginButton!.disabled = false;
+      //logic
+
     }
     
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated]);
 
-    const login = async () => {
-      if (!isAuthenticated) {
-
-        await authenticate({signingMessage: "Log in using Moralis" })
-          .then(function (user) {
-            console.log("Logged in user:\n\t", user!.get("ethAddress"));
-            console.log(user);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
+  const login = async () => {
+    
+    if (!isAuthenticated) {
+      await authenticate({signingMessage: "Log in using Moralis" })
+        .then(function (user) {
+          console.log("Logged in user:\n\t", user!.get("ethAddress"));
+          console.log(user);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
+  }
 
-    const logOut = async () => {
-      if (user){
-        console.log("Logged in as: \n\t",user.get("ethAddress"));
-      }
-      else{
-        console.log("User already logged out")
-      }
-      await logout();
-      console.log("Logging out DONE");
+  const logOut = async () => {
+    if (user){
+      console.log("Logged in as: \n\t",user.get("ethAddress"));
     }
+    else{
+      console.log("User already logged out")
+    }
+    await logout();
+    console.log("Logging out DONE");
+  }
 
   return (
     <div>
       <h1>Moralis Hello World!</h1>
-      <button id="login_btn" onClick={login}>Moralis Metamask Login</button>
-      <button id="logout_btn" onClick={logOut} disabled={isAuthenticating}>Logout</button>
+      <button id="login_btn" onClick={login} disabled={isAuthenticated || isAuthenticating}>Moralis Metamask Login</button>
+      <button id="logout_btn" onClick={logOut} disabled={!isAuthenticated ||isAuthenticating}>Logout</button>
     </div>
   );
 }
