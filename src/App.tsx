bebdbf0986f47c54440c8d54c10d64b44e2a55ec
zembahk <77575523+zembahk@users.noreply.github.com>
2022-08-04 00:56:25 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React, { useEffect, useState} from 'react'
-//import logo from './logo.svg'
 import './App.css'
 import { CONTRACT_ABI } from './abi'
-import { useMoralis } from 'react-moralis'
-import Moralis from 'moralis/types'
+import { useMoralis, useChain } from 'react-moralis'
 import ReactPlayer from 'react-player'
+
 
 function App() {
   const [name, setName] = useState('')
-  const [id, setId] = useState('')
+  const [id, setId] = useState('0')
   const [bamboo, setBamboo] = useState('')
   const [feed, setFeed] = useState('')
   const [fed, setFed] = useState(-1)
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
-  //const [image, setImage] = useState('')
   const [animate, setAnimate] = useState('')
   const [userAddress, setUserAddress] = useState('')
-  //const [hogeAddress, setHogeAddress] = useState('0xfad45e47083e4607302aa43c65fb3106f1cd7607') //eth chain
   const [hogePandasAddress] = useState('0x5Ea0333638b035BB911eD77F101C2bea979A2843') // polygon chain 
 
   const handleNameChange = (event: { target: { value: React.SetStateAction<string> } }) => {
@@ -34,47 +31,34 @@ function App() {
   const handleFeedChange = (event: { target: { value: React.SetStateAction<string> } }) => {
     setFeed(event.target.value)
   }
-
-  const { Moralis, authenticate, isAuthenticated, isAuthenticating, user, logout } = useMoralis()
-  
+  const { switchNetwork, chainId, chain } = useChain();
+  const { Moralis, authenticate, isAuthenticated, isAuthenticating, user, logout, isWeb3Enabled } = useMoralis()
+  const ethers = Moralis.web3Library
 
   useEffect(() => {
-    
-    if (isAuthenticated) {
+    if (isAuthenticated){
       setUserAddress(user!.get('ethAddress'))
-      //console.log(user)
       ethersContract()
     }
-    
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated])
+  }, [isAuthenticated, isWeb3Enabled, chain])
 
   const ethersContract = async () => {
-    const ethers = Moralis.web3Library
-    //const hogeContract = new ethers.Contract(hogeAddress, hogeAbiExample, await Moralis.enableWeb3())
-    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
-    //example calls
-    const name = await HogePandaContract.name()
-    const symbol = await HogePandaContract.symbol()
-    console.log(name, '\n', symbol)
-  }
-
-  const exeFunc = async (options: Moralis.ExecuteFunctionOptions) => {
-    try{
-      await Moralis.enableWeb3()
-      const result = await Moralis.executeFunction(options)
-      const json = JSON.parse(JSON.stringify(result))
-      return json
-    } catch(e) {
-      console.log('Error', e)
-      return { 'error': e }
+    const web3 = await Moralis.enableWeb3()
+    const Contract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, web3)
+    if (chainId !== '0x89' || chain === null) {
+      switchNetwork('0x89')
+      alert("Need to be on polygon network")
     }
+    const name = await Contract.name()
+    const symbol = await Contract.symbol()
+    console.log(name, '\n', symbol)
+    return Contract
   }
 
   const clearData = async () => {
     setTitle('')
     setDescription('')
-    //setImage('')
     setAnimate('')
     setFed(-1)
     setName('')
@@ -105,13 +89,8 @@ function App() {
   }
 
   const getTokenData = async () => {
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'getTokenData',
-      abi: CONTRACT_ABI,
-      params: { nft_id: parseInt(id) }
-    }
-    const result = await exeFunc(options)
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const result = await HogePandaContract.getTokenData(id)
     fetch(result)
     .then(response => response.json())
     .then((jsonData) => {
@@ -119,7 +98,6 @@ function App() {
       console.log(obj)
       setTitle(obj.title)
       setDescription(obj.description)
-      //setImage(obj.image)
       setAnimate(obj.animation_url)
     })
     .catch((error) => {
@@ -132,118 +110,55 @@ function App() {
   }
 
   const getTimesFed = async () => {
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'getTimesFed',
-      abi: CONTRACT_ABI,
-      params: { nft_id: parseInt(id) }
-    }
-    const result = await exeFunc(options)
-    setFed(parseInt(result.hex, 16))
-    console.log(result)
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const fed = await HogePandaContract.getTimesFed(id)
+    setFed(parseInt(fed._hex, 16))
   }
 
   const getTokenName = async () => {
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'getTokenName',
-      abi: CONTRACT_ABI,
-      params: { nft_id: parseInt(id) }
-    }
-    const result = await exeFunc(options)
-    if(result?.error){
-      clearData()
-    }else {
-      setName(result)
-      console.log(result)
-    }
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const name = await HogePandaContract.getTokenName(id)
+    setName(name)
+    console.log(name)
   }
 
   const newPanda = async () => {
     if (!userAddress){return}
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'createHogePanda',
-      abi: CONTRACT_ABI,
-      msgValue: Moralis.Units.ETH('0.025'),
-      params: { account: userAddress, nft_name: name }
-    }
-    const result = await exeFunc(options)
-    if(result?.error){
-      clearData()
-    }else {
-      console.log(result)
-    }
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const result = await HogePandaContract.getTokenName({value: ethers.utils.parseEther('0.025'), account: userAddress, nft_name: name })
+    console.log(result)
   }
   
   const createBamboo = async () => {
     if (!userAddress){return}
-    const price = parseInt(bamboo) * 0.01
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'createBamboo',
-      abi: CONTRACT_ABI,
-      msgValue: Moralis.Units.ETH(price),
-      params: { account: userAddress, amount: parseInt(bamboo) }
-    }
-    const result = await exeFunc(options)
-    if(result?.error){
-      clearData()
-    }else {
-      console.log(result)
-    }
+    const price = ethers.utils.parseEther((parseInt(bamboo) * 0.01).toString())
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const result = await HogePandaContract.createBamboo({value: price, account: userAddress, nft_name: name })
+    console.log(result)
   }
 
   const feedBamboo = async () => {
     if (!userAddress){return}
-    const price = (parseInt(feed) * 0.01)
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'feedBamboo',
-      abi: CONTRACT_ABI,
-      msgValue: Moralis.Units.ETH(price),
-      params: { account: userAddress, nft_id: parseInt(id), amount: parseInt(feed) }
-    }
-    const result = await exeFunc(options)
-    if(result?.error){
-      clearData()
-    }else {
-      console.log(result)
-    }
+    const price = ethers.utils.parseEther((parseInt(feed) * 0.01).toString())
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const result = await HogePandaContract.feedBamboo({value: price, account: userAddress, nft_id: id, amount: feed })
+    const fed = await HogePandaContract.getTimesFed(id)
+    setFed(parseInt(fed._hex, 16))
+    console.log(result)
   }
   
   const growToYoung = async () => {
     if (!userAddress){return}
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'growToYoung',
-      abi: CONTRACT_ABI,
-      msgValue: Moralis.Units.ETH('0.05'),
-      params: { account: userAddress, nft_id: parseInt(id) }
-    }
-    const result = await exeFunc(options)
-    if(result?.error){
-      clearData()
-    }else {
-      console.log(result)
-    }
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const result = await HogePandaContract.growToYoung({value: ethers.utils.parseEther('0.05'), account: userAddress, nft_id: id})
+    console.log(result)
   }
 
   const growToAdult = async () => {
     if (!userAddress){return}
-    const options = {
-      contractAddress: hogePandasAddress,
-      functionName: 'growToAdult',
-      abi: CONTRACT_ABI,
-      msgValue: Moralis.Units.ETH('0.1'),
-      params: { account: userAddress, nft_id: parseInt(id) }
-    }
-    const result = await exeFunc(options)
-    if(result?.error){
-      clearData()
-    }else {
-      console.log(result)
-    }
+    const HogePandaContract = new ethers.Contract(hogePandasAddress, CONTRACT_ABI, await Moralis.enableWeb3())
+    const result = await HogePandaContract.growToAdult({value: ethers.utils.parseEther('0.1'), account: userAddress, nft_id: id})
+    console.log(result)  
   }
 
   return (
@@ -253,11 +168,12 @@ function App() {
       <button id='logout_btn' onClick={logOut} disabled={!isAuthenticated}>Logout</button>
       <div id='address_div' >{userAddress}</div>
       <p></p>
-      <button id='clear_btn' onClick={clearData} >Clear Fields </button>
+      <button id='clear_btn' onClick={clearData} >Clear Fields</button>
+      &emsp;
+      <button id='data_btn' onClick={getTokenData} disabled={!isAuthenticated || !id || parseInt(id) < 0}>Fetch Token Data</button>
       <br></br>
       <label> Token ID: </label>
       <input id='input_id' type='number' name='id' onChange={handleIdChange} value={id} />
-      <button id='data_btn' onClick={getTokenData} disabled={!isAuthenticated || !id || parseInt(id) < 0}>Token Data </button>
       <br></br>
       <label> Name: </label>
       <input id='input_name' type='text' name='name' onChange={handleNameChange} value={name} autoFocus/>
@@ -266,6 +182,11 @@ function App() {
       <input id='input_title' type='text' name='title' value={title} disabled />
       <input id='input_description' type='text' name='description' value={description} disabled />
       <p></p>
+      <button id='young_btn' onClick={growToYoung} disabled={!isAuthenticated || !id 
+        || title !== 'New Hoge Panda' || fed < 1 || !fed }>Grow To Young 🐼</button>
+      <button id='adult_btn' onClick={growToAdult} disabled={!isAuthenticated || !id 
+        || title !== 'Young Hoge Panda' || fed < 3 || !fed}>Grow To Adult 🐼</button>
+      <br></br>
       <button id='fed_btn' onClick={getTimesFed} disabled={!isAuthenticated || !id}>Times Fed 🀤</button>
       <input id='times_fed' type='number' name='fed' value={fed} disabled />
       <br></br>
@@ -274,12 +195,7 @@ function App() {
       <br></br>
       <input id='input_feed' type='number' name='feed' onChange={handleFeedChange} value={feed}/>
       <button id='feed_btn' onClick={feedBamboo} disabled={!isAuthenticated || !feed || !id}>Feed 🐼 Bamboo 🀤</button>
-      <p></p>
-      <button id='young_btn' onClick={growToYoung} disabled={!isAuthenticated || !id 
-        || title !== 'New Hoge Panda' || fed < 1 || !fed }>Grow To Young 🐼</button>
-      <button id='adult_btn' onClick={growToAdult} disabled={!isAuthenticated || !id 
-        || title !== 'Young Hoge Panda' || fed < 3 || !fed}>Grow To Adult 🐼</button>
-      <p></p>
+      <p><br></br></p>
       <ReactPlayer playing url={animate} height='100%' controls={true} loop={true} />
     </div>
   )
